b43d974ee17ecc20ac34f9039bc1bbd67a3a1b2f
Ashraful Islam Sheiblu <sheiblu@yahoo.com>
2016-08-06 01:43:56 +0600

    Add files via upload

---
+++ Uri_1957.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Uri_1957 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		String s = String.format("%X", num);		
+		System.out.println(s);
+
+	}
+
+}
