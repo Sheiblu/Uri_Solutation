fbc50e2687e70c24693006de30a3a7ee1b67e0d2
Ashraful Islam Sheiblu <sheiblu@yahoo.com>
2016-12-27 21:23:38 +0600

    Add files via upload

---
+++ 2483.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+  int i , j, loop;
+
+  scanf("%d",&loop);
+
+  printf("Feliz nat");
+
+  for (i=0;i<loop;i++){
+    printf("a");
+  }
+
+  printf("l!\n");
+
+  return 0;
+
+}
